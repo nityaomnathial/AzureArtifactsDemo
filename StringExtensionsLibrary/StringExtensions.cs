0263193a01f915525064e097f2fce6b15040a363
Nityaom Nathial <[emailprotected]>
2025-03-31 19:46:20 -0400

    Initial commit for Azure DevOps CI/CD Lab

---
+++ StringExtensionsLibrary/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace StringExtensionsLibrary
+{
+    public static class StringExtensions
+    {
+        // Extension method for string to check if it starts with an uppercase letter
+        public static bool StartsWithUpper(this string str)
+        {
+            return !string.IsNullOrEmpty(str) && char.IsUpper(str[0]);
+        }
+    }
+}
---
+++ StringTestApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using StringExtensionsLibrary;
+
+class Program
+{
+    static void Main()
+    {
+        string word1 = "Hello";
+        string word2 = "world";
+
+        Console.WriteLine($"'{word1}' starts with uppercase? {word1.StartsWithUpper()}");
+        Console.WriteLine($"'{word2}' starts with uppercase? {word2.StartsWithUpper()}");
+    }
+}
